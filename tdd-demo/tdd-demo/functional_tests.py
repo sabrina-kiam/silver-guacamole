6824921ed4b839fa05ccb67659c9ed2212101aaa
sabrina kiamilev <sabrina.kiamilev@gmail.com>
2022-07-13 15:01:30 -0400

    front page html now generated from template

--- tdd-demo/tdd-demo/functional_tests.py
+++ tdd-demo/tdd-demo/functional_tests.py
@@ -24,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
-			' Enter a to-do item'
+			'Enter a to-do item'
 		)
 
 		# she types "buy feathers" into a text box
@@ -38,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			'New to-do item did not appear in table'
 		)
 
 		# there is a text box to add anotehr item.
