92fc6476ee51925003813af429086a3fe17d6afa
sabrina kiamilev <sabrina.kiamilev@gmail.com>
2022-07-13 15:40:48 -0400

    extract checking for table text into own helper method

--- tdd-demo/tdd-demo/functional_tests.py
+++ tdd-demo/tdd-demo/functional_tests.py
@@ -10,6 +10,13 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(
+			row_text, [row.text for row in rows]
+		)
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# We go to check out the homepage
 		self.browser.get('http://localhost:8000')
@@ -35,11 +42,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(
-			'1: Buy peacock feathers', [row.text for row in rows]
-		)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# there is a text box to add anotehr item.
 		# she enteres "use feathers to make a fly"
@@ -49,14 +52,8 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 
 		# page updates again, show both items in thel ist
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(
-			'1: Buy peacock feathers', [row.text for row in rows]
-		)
-		self.assertIn(
-			'2: Use peacock feathers to make fly', [row.text for row in rows]
-		)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
 
 		self.fail('finish the test!')
 
