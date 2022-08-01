6aad5b1e95ee6f64161c37a3cea6c9db6a89d944
sabrina kiamilev <sabrina.kiamilev@gmail.com>
2022-08-01 18:23:31 -0400

    add urls for creating a new list via post, move code into views and tests

--- tdd-demo/tdd-demo/lists/tests.py
+++ tdd-demo/tdd-demo/lists/tests.py
@@ -19,28 +19,25 @@ class ListViewTest(TestCase):
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		self.assertTemplateUsed(response, 'list.html')
 
-class HomePageTest(TestCase):
 
-	def test_uses_home_template(self):
-		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'home.html')
-
+class NewListTest(TestCase):
 	def test_can_save_post_request(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
 		
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 
 	def test_can_redirect_after_post(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-	def test_only_save_non_empty_items(self):
-		self.client.get('/')
-		self.assertEqual(Item.objects.count(), 0)
+
+class HomePageTest(TestCase):
+
+	def test_uses_home_template(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
 
 
 class ItemModelTest(TestCase):
