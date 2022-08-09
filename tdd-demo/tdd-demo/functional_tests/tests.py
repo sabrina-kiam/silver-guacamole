44716f17ee1243f6b7fddd9c76149ea5636870f7
sabrina kiamilev <sabrina.kiamilev@gmail.com>
2022-08-08 23:27:38 -0400

    first steps of ft for layout and styling

--- tdd-demo/tdd-demo/functional_tests/tests.py
+++ tdd-demo/tdd-demo/functional_tests/tests.py
@@ -29,6 +29,22 @@ class NewVisitorTest(LiveServerTestCase):
 					raise e
 				time.sleep(0.5)
 
+	def test_layout_and_styling(self):
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
+
 	def test_multiple_users_can_start_lists(self):
 		self.browser.get(self.live_server_url)
 		inputbox = self.browser.find_element_by_id('id_new_item')
